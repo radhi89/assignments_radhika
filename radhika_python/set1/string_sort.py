8f71e7ee52ca17c8f9553cf3e1129f3f58518ad7
radhi89 <51118067+radhi89@users.noreply.github.com>
2019-06-05 11:19:30 +0530

    Delete string_sort.py

--- radhika_python/set1/string_sort.py
+++
@@ -1,7 +0,0 @@
-"""Write a program that accepts a comma separated sequence of
-words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
-"""
-values = input("Enter the words:")
-li = values.split(',')
-li.sort()
-print("Sorted words are :",(',').join(li))
