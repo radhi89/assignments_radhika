04e207587337a844adeb2565be2b5030967d300d
radhi89 <51118067+radhi89@users.noreply.github.com>
2019-06-05 11:20:59 +0530

    Delete mobile.py

--- radhika_python/set3/mobile.py
+++
@@ -1,6 +0,0 @@
-#Write a program to find all mobile number inside a string.
-import re
-
-string = input("Enter the string: ")
-mobile_no = re.findall(r"[0-9]{10}",string)
-print("Mobile number inside string is : ",mobile_no)
